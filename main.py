75fcc17e76036d5173e2ab76272d5843826efeac
Mark Delchev <markdelchev@gmail.com>
2023-01-14 00:56:52 +0200

    print result only if it is win or draw

--- main.py
+++ main.py
@@ -7,16 +7,15 @@ match_instance = Match()
 teams_instance = Teams()
 
 team_name = input("Team name: ")
-academy_level = 30
+academy_level = 60
 team_strength = player_selection_instance.select_team(academy_level)
 budget = 1000000
 
 while True:
-    academy_level = 30
     rival_team = teams_instance.random_team()
 
-    print(f"Your team's rating is {team_strength} and you will be playing against\
- {teams_instance.r_team} with {teams_instance.r_rating} rating.")
+    #print(f"Your team's rating is {team_strength} and you will be playing against\
+ #{teams_instance.r_team} with {teams_instance.r_rating} rating.")
 
     """
     for i in range(100):
@@ -25,11 +24,14 @@ while True:
             print(result)
     """
     result = match_instance.simulate_match(f"{team_name}-{team_strength}", f"{rival_team}")
-    print(result)
+    #print(result)
     if match_instance.t1_goals >= match_instance.t2_goals:
+        print(f"Your team's rating is {team_strength} and you will be playing against\
+ {teams_instance.r_team} with {teams_instance.r_rating} rating.")
+        print(result)
         budget += 1000000
         print("Congratulations!")
-    print(f"Current budget: {budget:,}")
+        print(f"Current budget: {budget:,}")
     if budget >= 5000000:
         break
 
--- pygame_test.py
+++ pygame_test.py
@@ -57,6 +57,7 @@ teams_instance = Teams()
 match_instance = Match()
 rival_team = teams_instance.random_team()
 result = match_instance.simulate_match("Levski-57", f"{rival_team}")
+
 button1 = Button(
     "Play Match",
     (50, 100),
