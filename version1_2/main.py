bfb847999201fe4ff9b1f4ce417d0462c04b40a0
Mark Delchev <markdelchev@gmail.com>
2023-04-02 16:39:52 +0300

    commands to buy or recruit players

--- version1_2/main.py
+++ version1_2/main.py
@@ -3,14 +3,24 @@ from teams import *
 
 player_instance = Player()
 team_instance = Teams()
-academy = True
 
-for i in range(11):
-    player_instance.gen_player_name("England")
-    player_instance.gen_player_position()
-    player_instance.gen_player_stats(20, academy)
-print(player_instance.team_stats)
-team_instance.gen_team_stats(player_instance.team_stats)
-print(team_instance.team_aggression)
-print(team_instance.team_attack)
+while True:
+    command = input()
+    if command == "stop":
+        break
+    elif command == "academy":
+        for i in range(11):
+            player_instance.gen_player_name("England")
+            player_instance.gen_player_position()
+            player_instance.gen_player_stats(20, True)
 
+    elif command == "buy_player":
+        for i in range(11):
+            player_instance.gen_player_name("England")
+            player_instance.gen_player_position()
+            player_instance.gen_player_stats(20, False)
+
+    print(player_instance.team_stats)
+    team_instance.gen_team_stats(player_instance.team_stats)
+    print(team_instance.team_aggression)
+    print(team_instance.team_attack)
