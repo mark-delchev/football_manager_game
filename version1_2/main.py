4e139dba587867302d096db875a267e34297309b
Mark Delchev <markdelchev@gmail.com>
2023-04-03 12:40:54 +0300

    command to sort players by age

--- version1_2/main.py
+++ version1_2/main.py
@@ -13,14 +13,20 @@ while True:
             player_instance.gen_player_name("England")
             player_instance.gen_player_position()
             player_instance.gen_player_stats(20, True)
+            print(player_instance.player_stats)
 
     elif command == "buy_player":
         for i in range(11):
             player_instance.gen_player_name("England")
             player_instance.gen_player_position()
             player_instance.gen_player_stats(20, False)
+            print(player_instance.team_stats)
 
-    print(player_instance.team_stats)
-    team_instance.gen_team_stats(player_instance.team_stats)
-    print(team_instance.team_aggression)
-    print(team_instance.team_attack)
+    elif command == "sort_age":
+        unsorted_players = player_instance.team_stats
+        age_sorted = sorted(unsorted_players, key=lambda x: x['age'])
+        print(age_sorted)
+
+team_instance.gen_team_stats(player_instance.team_stats)
+print(team_instance.team_aggression)
+print(team_instance.team_attack)
