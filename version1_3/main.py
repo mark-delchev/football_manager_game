f054d58c26f96575cf8a3952e3c8fd55f255e95b
mark-delchev <markdelchev@gmail.com>
2023-06-27 16:35:17 +0300

    authentication

--- version1_3/main.py
+++ version1_3/main.py
@@ -1,17 +1,1 @@
-from flask import Flask, render_template
-import random
-
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-    names = generate_names()  # Replace generate_names() with your own function
-    return render_template('index.html', names=names)
-
-def generate_names():
-    # Your code to generate random names
-    names = ['Alice', 'Bob', 'Charlie']  # Replace with your actual name generation logic
-    return names
-
-if __name__ == '__main__':
-    app.run(debug=True)
+from website import create_app
