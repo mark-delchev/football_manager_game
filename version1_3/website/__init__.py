07ddc2efb8cf1ecc841cd57117b4db45c72d791f
mark-delchev <markdelchev@gmail.com>
2023-07-10 12:24:23 +0300

    routes set up

--- version1_3/website/__init__.py
+++ version1_3/website/__init__.py
@@ -4,5 +4,11 @@ def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = '12345671'
 
+    from .views import views
+    from .auth import auth
+
+    app.register_blueprint(views, url_prefix="/")
+    app.register_blueprint(auth, url_prefix="/")
+
     return app
 
